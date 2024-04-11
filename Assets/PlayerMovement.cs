9147ad06a8ce24f786faa2f9837773d4525f4fd7
Tran Nham <tranonunity@gmail.com>
2024-04-11 14:53:05 +0300

    Input Move&Jump, Gravity applied

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,8 +6,25 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [Header("Movement")]
     public float moveSpeed = 5f;
     float horizontalMovement;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+    public int maxJumps = 2;
+    int jumpsRemaining;
+
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+
+    [Header("Gravity")]
+    public float baseGravity = 2f;
+    public float maxFallSpeed = 18f;
+    public float fallSpeedMultiplier = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +35,43 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+        GroundCheck();
+        Gravity();
     }
 
+    private void Gravity() {
+        if(rb.velocity.y < 0) {
+            rb.gravityScale = baseGravity * fallSpeedMultiplier; //fall with acceleration
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed));
+        } else {
+            rb.gravityScale = baseGravity;
+        }
+    }
     public void Move(InputAction.CallbackContext context) {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    } 
+    public void Jump(InputAction.CallbackContext context) {
+        if (jumpsRemaining > 0) {
+            if (context.performed) {
+                //hold down = full power
+                rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+                jumpsRemaining--;
+            } else if (context.canceled) {
+                //light tap  = half power
+                rb.velocity = new Vector2(rb.velocity.x, 0.5f * jumpPower);
+                jumpsRemaining--;
+            }
+        }
+    }
+
+    private void GroundCheck() {
+        if(Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer)) {
+            jumpsRemaining = maxJumps;
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
     }
 }
